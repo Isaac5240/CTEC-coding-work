22b954088e4b9129ebf8bbaa18cb36eceafce51c
Isaac5240 <82936761+Isaac5240@users.noreply.github.com>
2022-08-15 10:01:13 +0800

    Update server.js

--- Manga_Website/server.js
+++ Manga_Website/server.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.route("/manga").get(mangaController.getAllManga);
 app.route("/character").get(characterController.getAllCharacters);
 
-app.listen(8080, "127.0.0.1");
-console.log("web server running @ http://127.0.0.1:8080");
+app.listen(3000, "127.0.0.1");
+console.log("web server running @ http://127.0.0.1:3000");
